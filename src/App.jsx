3d0cdf0d4433e222edfd83a11cca6bb28fc5703c
pingmeorayush <pingmeorayush@gmail.com>
2024-11-21 21:58:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+
+export default function App() {
+  return (
+    <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+      <Header />
+      <Hero />
+      <Benefits />
+      <Collaboration />
+    </div>
+  );
+}
